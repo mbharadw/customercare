ff0947538f48f8c3a6281270cfb6569d36d4dabd
MaheshB <bmb4791@gmail.com>
2020-08-06 08:45:50 +0000

    Added new README.md file and modified Helloworld.java file to add comments

--- Helloworld.java
+++ Helloworld.java
@@ -1,3 +1,4 @@
+//This program will print simple message
 public static void main(String args[]){
 
 	System.out.println("Hello world");
---
+++ README.md
@@ -0,0 +1,7 @@
+Applicaion Name: Customer Care
+
+Platform: Linux
+
+Language: Java
+
+team members: Dev A, Dev B
