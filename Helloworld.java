b75ad2311d896330a26c9562ddf3e54729e6d888
MaheshB <bmb4791@gmail.com>
2020-08-09 09:27:07 +0000

    Changed as a part of facebook-int project

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,9 @@
 //This program will print simple message saying hello world and welcoming to git
 public static void main(String args[]){
 
+	//prints hello world in the terminal window
 	System.out.println("Hello world");
-	System.out.println("Welcome to Git");
+	//prints welcome to the facebook in the terminal window
+	System.out.println("Welcome to facebook");
 
 }
