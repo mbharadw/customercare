ad76c5108405523c1f0bcf11406de95983b48b52
MaheshB <bmb4791@gmail.com>
2020-08-09 06:29:53 +0000

    Added detail comments

--- Helloworld.java
+++ Helloworld.java
@@ -1,4 +1,4 @@
-//This program will print simple message
+//This program will print simple message saying hello world and welcoming to git
 public static void main(String args[]){
 
 	System.out.println("Hello world");
